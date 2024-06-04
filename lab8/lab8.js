e32cc2a0a19991422337f84d7b8bc4f6b1d2253a
Плюснина Елизавета <elizaplus@list.ru>
2024-06-05 02:30:47 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -16,4 +16,13 @@ function showDate () {  // функция выводит текущую дату
     for (let [country, locale] of Object.entries(locales)) {
         localeDates.innerHTML += `Дата и время ${country}: ${today.toLocaleString(locale)}<br>`;
     }
+
+    let additionalInfo = document.getElementById('additional-info');
+    let daysOfWeek = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
+    additionalInfo.innerHTML = `
+        Текущий год: ${today.getFullYear()}<br>
+        Текущий месяц: ${today.toLocaleString('ru-RU', { month: 'long' })}<br>
+        Текущая дата: ${today.getDate()}<br>
+        День недели: ${daysOfWeek[today.getDay()]}`;
 }
+
