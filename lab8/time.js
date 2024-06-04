f871831b52dd5d54f1ae82d33d39972f8a5b702d
Плюснина Елизавета <elizaplus@list.ru>
2024-06-05 03:11:58 +0700

    Время

---
+++ lab8/time.js
@@ -0,0 +1,21 @@
+function showTime() {
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-RU');
+    document.getElementById('time').innerHTML = currentTime;
+}
+setInterval(showTime, 1000);
+
+let totalDegrees = 0;
+
+    function showTime() {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        const timeString = `${hours}:${minutes}:${seconds}`;
+        
+        document.getElementById('time').textContent = timeString;
+        document.getElementById('seconds').textContent = seconds;
+    }
+
+setInterval(showTime, 1000);
